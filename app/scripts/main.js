8944ff15eea67e1ebe51be77fc2b638408dc429f
Shannon Wynn <shannonwynn@mac.com>
2014-10-17 17:57:19 -0400

    JS file is working again, login window added.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,35 @@
 var template = _.template($('.chat-output-template').html());
 var apiUrl = "http://tiny-pizza-server.herokuapp.com/collections/JS-Chat";
+var formObject = {};
+
+/* user login*/
+$('.login-form [type=submit]').on('click', function (event) {
+  event.preventDefault();
+  var loginValues = $('input.userName').serializeArray();
+
+   loginValues.forEach(function (userName) {
+    formObject[userName.name] = userName.value;
+  });
+});
+/* end user login*/
 
 $('input[type=submit]').on('click', function (event) {
   event.preventDefault();
   var fieldValues = $('input.field').serializeArray();
-  var formObject = {}
 
    fieldValues.forEach(function (field) {
     formObject[field.name] = field.value;
   });
 
 //console.log(formObject);
-  formObject['name'] = ' ';
-  formObject['picture'] = ' ';
-  formObject['date'] = ' ';
+  formObject.picture = ' ';
+  formObject.date = ' ';
 
   $.ajax({
     method: 'POST',
     url: apiUrl,
     data: formObject
-  }).done(function (data) { $('input.field').val(' ') });
+  }).done(function (data) { $('input.field').val(' '); });
 });
 
 var previousCount = 0;
@@ -34,6 +44,6 @@ setInterval(function () {
       });
 
       $('.chat-output').html(finishedTemplates);
-    };
+    }
   });
 }, 1000);
